ca2fd48bfa898ed51d57b341e50a04472fec1f5c
t <tschultztt@gmail.com>
2014-10-30 13:11:18 -0500

    Added active link. not sure about this one

--- client/Helpers/globalSessions.js
+++ client/Helpers/globalSessions.js
@@ -1,3 +1,8 @@
 
 //Current Page
-Session.set('currentPage', '');
+
+curPath=function(){var c=window.location.pathname;var b=c.slice(0,-1);var a=c.slice(-1);if(b==""){return"/"}else{if(a=="/"){return b}else{return c}}};
+
+Template.registerHelper('active', function (path) {
+  return curPath() == path ? 'active' : '';
+});
